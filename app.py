86b100df9ef20445767ea7afcd352d9c37f30555
issam20 <35964120+issam20@users.noreply.github.com>
2021-01-28 21:32:11 +0100

    first

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask,jsonify
+from flask_restful import Api, Resource
+from flask_cors import CORS, cross_origin
+import os
+import sys
+
+data = None
+app = Flask(__name__)
+cors = CORS(app)
+api = Api(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+def init_webhooks(base_url):
+    # Update inbound traffic via APIs to use the public-facing ngrok URL
+    pass
+
+def create_app():
+    
+    # Initialize our ngrok settings into Flask
+    app.config.from_mapping(
+        BASE_URL="http://localhost:5000",
+        USE_NGROK=os.environ.get("USE_NGROK", "False") == "True" and os.environ.get("WERKZEUG_RUN_MAIN") != "true"
+    )
+
+    if app.config.get("ENV") == "development" and app.config["USE_NGROK"]:
+        # pyngrok will only be installed, and should only ever be initialized, in a dev environment
+        from pyngrok import ngrok
+
+        # Get the dev server port (defaults to 5000 for Flask, can be overridden with `--port`
+        # when starting the server
+        port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 5000
+
+        # Open a ngrok tunnel to the dev server
+        public_url = ngrok.connect(port).public_url
+        print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+
+        # Update any base URLs or webhooks to use the public ngrok URL
+        app.config["BASE_URL"] = public_url
+        init_webhooks(public_url)
+
+    # ... Initialize Blueprints and the rest of our app
+
+    return app
+
+class Settings(Resource):
+    @cross_origin()
+    def post(self, param):
+          return {
+        'status' : 'done',
+        'value' : param
+        }
+
+class Data(Resource):
+    @cross_origin()
+    def get(self):
+        return jsonify(data)
+class Root(Resource):
+    @cross_origin()
+    def get(self):
+          return "= Hawkeye ="
+     
+
+api.add_resource(Data, "/data")
+api.add_resource(Root, "/")
+api.add_resource(Settings, "/settings/<string:param>")
+
+def start():
+    app.run(debug=True)
+    
---
+++ main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 15:53:20 2021
+
+@author: tebib
+"""
+import cv2
+from GazeTracking import game
+import app as api
+if __name__ == '__main__':
+    # create an interactive window
+    game = game.Game()
+    # start the API
+    api.start()
+    
+    while True:
+        # update the interactive window frame
+        game.run_game()
+        # update the data to send to the API
+        api.data = game.to_dict()
+        # display the interactive window
+        
+        # Press Esc to quit the video analysis loop
+        if cv2.waitKey(1) == 27:
+            # Release video capture
+            cv2.destroyAllWindows()
+            break
